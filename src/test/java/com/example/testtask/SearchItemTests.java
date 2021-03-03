40da28fa897a98405b696db67c316eeccefecb4a
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-03-02 23:50:33 -0500

    Yandex project started

--- src/test/java/com/example/testtask/SearchItemTests.java
+++ src/test/java/com/example/testtask/SearchItemTests.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchItemTests {
     WebDriver wd;
@@ -13,28 +17,38 @@ public class SearchItemTests {
         wd = new ChromeDriver();
         wd.manage().window().maximize();
         openSite("https://yandex.ru/");
+    }
 
+    @Test
+    public void testItem() throws InterruptedException {
         click(By.cssSelector("[data-id='market']"));
+
+        switchToNextTab();
+
         click(By.cssSelector("[href*='/kompiuternaia-tekhnika']"));
 
-        click(By.cssSelector("[href*='/catalog--planshety']"));
+
+        click(By.cssSelector("[href^='/catalog--planshety/']"));
 
         type(By.cssSelector("#glpricefrom"), "20000");
         type(By.cssSelector("#glpriceto"), "35000");
         click(By.cssSelector("[name$=Apple]"));
-        wd.findElement(By.xpath("//*[@data-autotest-id='product-snippet'][2]"));
+
+//        WebElement item = wd.findElement(By.xpath("//*[@data-autotest-id='product-snippet'][2]"));
+//        String itemName = item.findElement(By.xpath(".//h3")).getText();
+
+        String itemElement = wd.findElement(By.xpath("//*[@data-autotest-id='product-snippet'][2]//h3")).getText();
+
 
     }
-
-
-
-
 
 
     public void type(By locator, String text) {
         click(locator);
         wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        if (text != null) {
+            wd.findElement(locator).sendKeys(text);
+        }
     }
 
     public void openSite(String url) {
@@ -44,5 +58,13 @@ public class SearchItemTests {
     public void click(By locator) {
         wd.findElement(locator).click();
     }
+
+    private void switchToNextTab() {
+        List<String> avaliableWindows = new ArrayList<>(wd.getWindowHandles());
+        if (!avaliableWindows.isEmpty()){
+            wd.switchTo().window(avaliableWindows.get(1));
+        }
+    }
+
 
 }
